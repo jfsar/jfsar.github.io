ed6e5752088013050fff322fa9a634bef40cd746
John <john.sarmiento452@gmail.com>
2026-01-14 14:53:41 +0800

    chore: use static image for social media tags

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Projects } from "./components/projects";
 import Skills from "./components/skills";
 import { Toaster } from "./components/ui/sonner";
 import {Helmet} from 'react-helmet';
+import { OGImage } from "./images";
 
 export default function App() {
   return (
@@ -13,7 +14,7 @@ export default function App() {
       <meta property="og:type" content="website" />
       <meta property="og:title" content="JS Portfolio" />
       <meta property="og:description" content="Web developer with 3 years of experience bringing design templates to life using React, HTML/CSS, and JavaScript. Explore my personal projects and experiments." />
-      <meta property="og:image" content="https://utfs.io/f/VD1WLwsKlNgDcbkXphzBKdsGCXO9ALkn7p4miH3WuS6bojxF"/>
+      <meta property="og:image" content={OGImage}/>
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:url" content="https://jfsar.github.io/" />
@@ -22,7 +23,7 @@ export default function App() {
       <meta property="twitter:url" content="https://jfsar.github.io/" />
       <meta property="twitter:title" content="JS Portfolio" />
       <meta property="twitter:description" content="Web developer with 3 years of experience bringing design templates to life using React, HTML/CSS, and JavaScript. Explore my personal projects and experiments." />
-      <meta property="twitter:image" content="https://utfs.io/f/VD1WLwsKlNgDcbkXphzBKdsGCXO9ALkn7p4miH3WuS6bojxF" />
+      <meta property="twitter:image" content={OGImage} />
      </Helmet>
       <div className="dark min-h-screen bg-background text-foreground">
         <Navigation />
