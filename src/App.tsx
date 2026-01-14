5d90ec8326a0bef288e934a2be8a341d6c3f2184
John <john.sarmiento452@gmail.com>
2026-01-14 14:59:07 +0800

    update public assets

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
       <meta property="og:type" content="website" />
       <meta property="og:title" content="JS Portfolio" />
       <meta property="og:description" content="Web developer with 3 years of experience bringing design templates to life using React, HTML/CSS, and JavaScript. Explore my personal projects and experiments." />
-      <meta property="og:image" content={OGImage}/>
+      <meta property="og:image" content={`${OGImage}`}/>
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:url" content="https://jfsar.github.io/" />
@@ -23,7 +23,7 @@ export default function App() {
       <meta property="twitter:url" content="https://jfsar.github.io/" />
       <meta property="twitter:title" content="JS Portfolio" />
       <meta property="twitter:description" content="Web developer with 3 years of experience bringing design templates to life using React, HTML/CSS, and JavaScript. Explore my personal projects and experiments." />
-      <meta property="twitter:image" content={OGImage} />
+      <meta property="twitter:image" content={`${OGImage}`} />
      </Helmet>
       <div className="dark min-h-screen bg-background text-foreground">
         <Navigation />
