a131ae6bcaefbcfcefdfd3b0d2af77c20634fee7
John <john.sarmiento452@gmail.com>
2026-01-14 15:54:12 +0800

    chore: change image for opengraph

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,30 @@ import Navigation  from "./components/navigation";
 import { Projects } from "./components/projects";
 import Skills from "./components/skills";
 import { Toaster } from "./components/ui/sonner";
+import {Helmet} from 'react-helmet';
+import OGImage from './assets/projects/og.png';
 
 export default function App() {
   return (
+    <>
+     <Helmet>
+      <title>John Sarmiento</title>
+      <meta name="title" content="JS Portfolio" />
+      <meta name="description" content="Web developer with 3 years of experience bringing design templates to life using React, HTML/CSS, and JavaScript. Explore my personal projects and experiments." />
+      <meta name="author" content="John Sarmiento" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="JS Portfolio" />
+      <meta property="og:description" content="Web developer with 3 years of experience bringing design templates to life using React, HTML/CSS, and JavaScript. Explore my personal projects and experiments." />
+      <meta property="og:image" content={OGImage}/>
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:url" content="https://jfsar.github.io/" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://jfsar.github.io/" />
+      <meta property="twitter:title" content="JS Portfolio" />
+      <meta property="twitter:description" content="Web developer with 3 years of experience bringing design templates to life using React, HTML/CSS, and JavaScript. Explore my personal projects and experiments." />
+      <meta property="twitter:image" content={OGImage}/>
+     </Helmet>
       <div className="dark min-h-screen bg-background text-foreground">
         <Navigation />
         <main>
@@ -14,8 +35,9 @@ export default function App() {
           <Skills />
           <Projects />
           <Contact />
-          <Toaster />
         </main>
+        <Toaster />
       </div>
+    </>
   );
 }
