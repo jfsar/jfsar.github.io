442caac8eb8ee6555c9ffe468304a911f1b234a1
John <john.sarmiento452@gmail.com>
2026-01-14 15:29:06 +0800

    chore:update workflow file

--- src/images/index.tsx
+++ src/images/index.tsx
@@ -2,13 +2,11 @@ import UrbanCapImage from '../assets/projects/urban_cap.png';
 import DashCoinImage from '../assets/projects/dash_coin.png';
 import QuizAppImage from '../assets/projects/react-quiz.png';
 import JFSARImage from '../assets/projects/jfsar.png';
-import OGImage from '../assets/og-image.jpg';
 
 
 export{
     UrbanCapImage,
     DashCoinImage,
     QuizAppImage,
-    JFSARImage,
-    OGImage
+    JFSARImage
 };
